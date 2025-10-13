e227a75bec85569b6e6d12cb71f46a9d0015f472
JONGHAN <howeer15325@naver.com>
2025-10-13 15:47:58 +0900

    Initial commit

---
+++ src/main/java/com/kt/techup/chatpg/ChatpgApplication.java
@@ -0,0 +1,13 @@
+package com.kt.techup.chatpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kt/techup/chatpg/ChatpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.techup.chatpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
