0c559d5fca798dc5180f226f75807f64fddb3a4b
JONGHAN <howeer15325@naver.com>
2025-10-14 12:27:52 +0900

    장비에 인벤토리 귀속 장착 장비 Map변경

--- src/main/java/com/kt/techup/chatpg/player/Player.java
+++ src/main/java/com/kt/techup/chatpg/player/Player.java
@@ -1,46 +1,31 @@
 package com.kt.techup.chatpg.player;
 
+import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+
 import com.kt.techup.chatpg.common.Character;
 
+import lombok.Getter;
+
+@Getter
 public class Player extends Character {
 
-	int exp;
-	private List<Equipment> inventory;  // 플레이어 인벤토리
-	Equipment weapon;
-	Equipment helmet;
-	Equipment boots;
-	Equipment armor;
-	Equipment accessory;
+	private int exp;
+	private final List<Equipment> inventory;
+	private final EquipmentManager equipmentManager;
 
 	public Player(String name, int level, int hp, int attack, int defense) {
 		super(name, level, hp, attack, defense);
-		this.exp = 0;
-	}
-
-	public void equip(Equipment weapon, Equipment helmet, Equipment boots, Equipment armor, Equipment accessory) {
-		this.weapon = weapon;
-		this.helmet = helmet;
-		this.boots = boots;
-		this.armor = armor;
-		this.accessory = accessory;
-	}
-
-	public void showEquip() {
-		System.out.println("=== 장비 정보 ===");
-		System.out.println("무기: " + weapon.getName() + " (ATK +" + weapon.getAttackBoost() + ", HP +" + helmet.getHpBoost() + ")");
-		System.out.println("헬멧: " + helmet.getName() + " (DEF +" + helmet.getDefenseBoost() + ", HP +" + helmet.getHpBoost() + ")");
-		System.out.println("갑옷: " + armor.getName() + " (DEF +" + armor.getDefenseBoost() + ", HP +" + armor.getHpBoost() + ")");
-		System.out.println("신발: " + boots.getName() + " (DEF +" + boots.getDefenseBoost() + ", HP +" + boots.getHpBoost() + ")");
-		System.out.println("악세사리: " + accessory.getName() + " (ATK +" + accessory.getAttackBoost() + ", HP +" + accessory.getHpBoost() + ")");
-		System.out.println("================");
+		this.inventory = new ArrayList<>();
+		this.equipmentManager = new EquipmentManager(inventory);
 	}
 
 	public void showStatus() {
 		System.out.println("=== 내 정보 ===");
 		System.out.println(name + " [Lv." + level + "] HP: " + hp + " ATK: " + attack + " DEF: " + defense);
 		System.out.println("================");
-
 	}
 
 	@Override
