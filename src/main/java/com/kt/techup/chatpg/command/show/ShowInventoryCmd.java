bcc081382a703578f7055c03160d8b7ee7b74769
JONGHAN <howeer15325@naver.com>
2025-10-17 17:30:50 +0900

    manager service 분리

--- src/main/java/com/kt/techup/chatpg/command/show/ShowInventoryCmd.java
+++ src/main/java/com/kt/techup/chatpg/command/show/ShowInventoryCmd.java
@@ -3,14 +3,26 @@ package com.kt.techup.chatpg.command.show;
 import com.kt.techup.chatpg.command.Command;
 import com.kt.techup.chatpg.common.GameContext;
 import com.kt.techup.chatpg.domain.player.Player;
+import com.kt.techup.chatpg.service.InventoryService;
+import com.kt.techup.chatpg.state.StateEnum;
+import com.kt.techup.chatpg.state.StateFactory;
 import com.kt.techup.chatpg.state.main.InventoryState;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class ShowInventoryCmd implements Command {
+
+	private final InventoryService inventoryService;
 
 	@Override
 	public void execute(Player player, GameContext context) {
 		System.out.println("ShowInventoryCmd");
-		player.getEquipmentManager().showInventory();
+
+		context.setCurrentState(StateFactory.getInstance().getState(StateEnum.INVENTORY));
+
+		inventoryService.showInventory(player);
+
 	}
 
 }
