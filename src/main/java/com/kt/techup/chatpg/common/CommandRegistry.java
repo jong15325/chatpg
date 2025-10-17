c9cecc5b8e8718d91b196b58a70a4dfd7b99f110
JONGHAN <howeer15325@naver.com>
2025-10-17 18:33:02 +0900

    component

--- src/main/java/com/kt/techup/chatpg/common/CommandRegistry.java
+++ src/main/java/com/kt/techup/chatpg/common/CommandRegistry.java
@@ -3,24 +3,32 @@ package com.kt.techup.chatpg.common;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
+
 import com.kt.techup.chatpg.command.Command;
 import com.kt.techup.chatpg.command.move.MoveDungeonCmd;
 import com.kt.techup.chatpg.command.show.ShowEquippedCmd;
 import com.kt.techup.chatpg.command.show.ShowInventoryCmd;
 import com.kt.techup.chatpg.command.show.ShowStatusCmd;
+import com.kt.techup.chatpg.service.InventoryService;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class CommandRegistry {
+
+	private final InventoryService inventoryService;
+
 	private final Map<String, Command> commandMap = new HashMap<>();
 
-	public CommandRegistry() {
-		registerCommands();
-	}
-
+	@PostConstruct
 	private void registerCommands() {
 		// Common commands
 		commandMap.put("info", new ShowStatusCmd());
 		commandMap.put("eq", new ShowEquippedCmd());
-		commandMap.put("inven", new ShowInventoryCmd());
+		commandMap.put("inven", new ShowInventoryCmd(inventoryService));
 
 		// Battle commands
 		//commandMap.put("attack", new AttackCmd());
