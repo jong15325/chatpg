2d1596abe3be9a01c77def36caf02896b42295c2
JONGHAN <howeer15325@naver.com>
2025-10-20 15:39:59 +0900

    커맨트 패턴 삭제 -> state패턴으로 통합

--- src/main/java/com/kt/techup/chatpg/command/Command.java
+++
@@ -1,8 +0,0 @@
-package com.kt.techup.chatpg.command;
-
-import com.kt.techup.chatpg.common.GameContext;
-import com.kt.techup.chatpg.domain.player.Player;
-
-public interface Command {
-	void execute(Player player, GameContext context);
-}
--- src/main/java/com/kt/techup/chatpg/state/StateFactory.java
+++ src/main/java/com/kt/techup/chatpg/state/StateFactory.java
@@ -3,6 +3,8 @@ package com.kt.techup.chatpg.state;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
+
 import com.kt.techup.chatpg.state.main.EquipmentState;
 import com.kt.techup.chatpg.state.main.InventoryState;
 import com.kt.techup.chatpg.state.main.MainState;
@@ -10,50 +12,22 @@ import com.kt.techup.chatpg.state.main.PlayerStatusState;
 
 import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
+@Component
 public class StateFactory {
 
-	private static StateFactory instance;
-	private final Map<StateEnum, GameState> stateCache;
-
-	private StateFactory() {
-		this.stateCache = new HashMap<>();
-		initializeStates();
-	}
-
-	public static StateFactory getInstance() {
-		if (instance == null) {
-			instance = new StateFactory();
-		}
-		return instance;
-	}
-
-	private void initializeStates() {
-		stateCache.put(StateEnum.MAIN, new MainState());
-		stateCache.put(StateEnum.PLAYER_STATUS, new PlayerStatusState());
-		stateCache.put(StateEnum.INVENTORY, new InventoryState());
-		stateCache.put(StateEnum.EQUIPMENT, new EquipmentState());
-
-		// 추가 상태들
-		// stateCache.put(StateType.DUNGEON, new DungeonState());
-		// stateCache.put(StateType.COMBAT, new CombatState());
-		// stateCache.put(StateType.SHOP, new ShopState());
-	}
+	private final MainState mainState;
+	private final EquipmentState equipmentState;
+	private final InventoryState inventoryState;
+	private final PlayerStatusState playerStatusState;
 
 	public GameState getState(StateEnum type) {
-		GameState state = stateCache.get(type);
-		if (state == null) {
-			throw new IllegalArgumentException("등록되지 않은 상태 타입: " + type);
-		}
-		return state;
+		return switch (type) {
+			case MAIN -> mainState;
+			case INVENTORY -> inventoryState;
+			case EQUIPMENT -> equipmentState;
+			case PLAYER_STATUS -> playerStatusState;
+			default -> throw new IllegalArgumentException("등록되지 않은 상태 타입: " + type);
+		};
 	}
-
-	public void registerState(StateEnum type, GameState state) {
-		stateCache.put(type, state);
-	}
-
-	public void clearCache() {
-		stateCache.clear();
-		initializeStates();
-	}
-
 }
