0eda5fdbf80f2bed39b407e1ba7d1c76d6318bf9
JONGHAN <howeer15325@naver.com>
2025-10-20 16:43:38 +0900

    .

--- src/main/java/com/kt/techup/chatpg/helper/CommandHelper.java
+++ src/main/java/com/kt/techup/chatpg/helper/CommandHelper.java
@@ -5,6 +5,7 @@ import com.kt.techup.chatpg.common.CommandEnum;
 public class CommandHelper {
 
 	public static void commandList(String filterType) {
+		PrintHelper.centerAlignPt("[가능한 명령어 리스트]");
 		for (
 			CommandEnum commandEnum : CommandEnum.values()) {
 			if (commandEnum.getType().equalsIgnoreCase(filterType)) {
